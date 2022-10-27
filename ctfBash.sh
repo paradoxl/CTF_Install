056f41688a7e1292adb16bd702cebdce1ba7a2a6
Michael Evans <Michael.evans1775@outlook.com>
2022-10-27 09:00:25 -0600

    wip BP pybluemonday

--- ctfBash.sh
+++ ctfBash.sh
@@ -2,6 +2,7 @@
 mkdir bashTesting
 cd bashTesting
 
+pip install --upgrade pip
 echo "Charging flux capacitor"
 echo "..."
 echo "Charging flux capacitor"
@@ -13,20 +14,20 @@ echo "Charging flux capacitor"
 
 echo "Engage git engines"
 git clone https://github.com/CTFd/CTFd.git
-clear
+
 cd CTFd
 echo "Installing requirements..."
 pip install -r requirements.txt
-clear
 
-cd bashTesting
+
+cd ..
 echo "Engage git engines"
 git clone https://github.com/paradoxl/STACCBOT.git
-clear
+
 cd STACCBOT
 echo "Installing requirements..."
 pip install -r requirements.txt
-clear
+
 
 
 cd CTFd
@@ -35,11 +36,11 @@ cd plugins
 
 echo "Engage git engines"
 git clone https://github.com/paradoxl/ctfd-recaptcha-plugin.git
-clear
+
 cd ctfd-recaptcha-plugin
 echo "Installing requirements..."
 pip install -r requirements.txt
-clear
+
 
 
 cd ..
@@ -50,14 +51,16 @@ echo "Engage git engines"
 
 git clone https://github.com/hmrserver/CTFd-theme-pixo.git
 
-clear
+
 
 echo "All the bits and bobs are there. Move to the readMe and configure the modules!"
 echo "Notes:"
 echo "ReCaptcha: default recaptcha keys have been preloaded. you will need to create you own"
-echo "building the CTF" 
-echo "Please follow the readMe and use ```sudo docker-compose up```"
+echo "building the CTF"
+
 cd ..
 cd CTFd
 
-sudo docker-compose build
+
+#broken packages
+#pybluemondaygit
